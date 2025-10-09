55c69489c7fa041ab8d8897359bd01dd6ffee2ce
Sebastian Zamora <zam98qui@gmail.com>
2025-10-09 14:17:50 -0500

    nuevo commit

---
+++ practica-1/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Message } from './components/Message'
+
+function App() {
+  return (
+    <>
+      <Message/>
+    </>
+  )
+}
+
+export default App

9e968b21f89eb79c3e8bfa5c0c0aed3575a74799
Sebastian Zamora <zam98qui@gmail.com>
2025-10-23 14:13:29 -0500

    feat: envolver LoginPage con AuthProvider para gestión de autenticación

--- 02-tailwindcss/src/App.tsx
+++ 02-tailwindcss/src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
 import { Counter } from './components/Counter'
 import { LoginPage } from './components/LoginPage'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
 
   return (
-    <>
-      <Counter />
+    // <>
+    //   <Counter />
+    // </>
+
+    // Usamos el AuthProvider para envolver los componentes que necesiten el contexto
+    <AuthProvider>
       <LoginPage />
-    </>
+    </AuthProvider>
   )
 }
 
